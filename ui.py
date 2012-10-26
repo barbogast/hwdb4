557b5fd91b492460dd4ba4b27fde54e5fde45464
Koblaid <koblaid@gmail.com>
2012-10-26 15:51:14 +0200

    Added navbar, restructured pages

--- ui.py
+++ ui.py
@@ -18,13 +18,17 @@ def shutdown_session(exception=None):
 
 @app.route("/")
 def index():
-    root_parts = db_session.query(M.Part).filter_by(parent_part=None)
-    return render_template('index.html', root_parts=root_parts)
+    return render_template('index.html')
 
-@app.route("/part")
-def part():
-    part = db_session.query(M.Part).filter_by(id=request.args['id']).one()
-    return render_template('part.html', part=part)
+@app.route("/parts")
+def parts():
+    if 'id' in request.args:
+        part = db_session.query(M.Part).filter_by(id=request.args['id']).one()
+        return render_template('parts_detail.html', part=part)
+    else:
+        root_parts = db_session.query(M.Part).filter_by(parent_part=None)
+        return render_template('parts.html', root_parts=root_parts)
+
 
 if __name__ == "__main__":
     app.run()
