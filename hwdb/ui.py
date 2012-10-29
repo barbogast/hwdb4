f87340f236dddd0bca77d11788fd3a970694a391
Koblaid <koblaid@gmail.com>
2012-10-29 13:27:53 +0100

    Added breadcrumb for part hierarchy on page part_detail.html

--- hwdb/ui.py
+++ hwdb/ui.py
@@ -4,6 +4,7 @@ Author: Benjamin Arbogast
 
 from flask import Blueprint, render_template, request
 from sqlalchemy.orm import scoped_session, sessionmaker
+from flaskext.htmlbuilder import html as H
 
 import hwdb.model as M
 
@@ -18,7 +19,24 @@ def index():
 def parts():
     if 'id' in request.args:
         part = M.db_session.query(M.Part).filter_by(id=request.args['id']).one()
-        return render_template('parts_detail.html', part=part)
+        has_parent = bool(part.parent_part)
+
+        # Generate breadcrumb for part
+        li_elements = []
+        divider = H.span(class_='divider')(H.i(class_='icon-chevron-right')(), ' ')
+
+        parent_part = part
+        while parent_part:
+            li = [H.a(href='/part?id=%s' % parent_part.id)(parent_part.name)]
+            # Omit the divider for the first elemement
+            if parent_parts_html:
+                li.append(divider)
+            li_elements.append(H.li(li))
+            parent_part = parent_part.parent_part
+
+        chain = H.ul(class_='breadcrumb')(reversed(li_elements))
+
+        return render_template('parts_detail.html', part=part, parent_part_chain=chain)
     else:
         root_parts = M.db_session.query(M.Part).filter_by(parent_part=None)
         return render_template('parts.html', root_parts=root_parts)
